08f9fe180ded2790754bedb9afd855ba9428791d
koddan2 <97536212+koddan2@users.noreply.github.com>
2022-11-15 20:30:55 +0100

    gui stuff etc

---
+++ source/dztool/DZT/DZT.Gui/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZT.Gui
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SetDayzServerRootDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FolderPicker();
+            if (picker.ShowDialog() is true)
+            {
+                DayzServerRootDirTextBox.Text = picker.ResultPath;
+            }
+        }
+
+        private void SetMpMissionFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FolderPicker { InputPath=DayzServerRootDirTextBox.Text};
+            if (picker.ShowDialog() is true)
+            {
+                MpMissionNameTextBox.Text = picker.ResultPath;
+            }
+        }
+    }
+}
