a5028c64fd4f00cac3a7c38046e86fdb949111f7
koddan2 <97536212+koddan2@users.noreply.github.com>
2022-11-15 22:34:16 +0100

    yep

--- source/dztool/DZT/DZT.Gui/MainWindow.xaml.cs
+++ source/dztool/DZT/DZT.Gui/MainWindow.xaml.cs
@@ -13,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IOPath = System.IO.Path;
+using IOFile = System.IO.File;
+using IODir = System.IO.Directory;
+using System.Text.Json;
 
 namespace DZT.Gui
 {
@@ -21,9 +25,56 @@ namespace DZT.Gui
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string _prefsJsonPath;
+
+        class AppPrefs
+        {
+            private readonly string _path;
+            public AppPrefs(string path) => _path = path;
+
+            public string DayzServerRootDirectoryPath { get; set; } = "";
+            public string MpMissionName { get; set; } = "";
+
+            public void Load()
+            {
+                var loaded = JsonSerializer.Deserialize<AppPrefs>(IOFile.ReadAllText(_path))
+                     ?? throw new ApplicationException("");
+
+                this.DayzServerRootDirectoryPath = loaded.DayzServerRootDirectoryPath;
+                this.MpMissionName = loaded.MpMissionName;
+            }
+
+            public void Store()
+            {
+                IOFile.WriteAllText(_path, JsonSerializer.Serialize(this, new JsonSerializerOptions { WriteIndented = true }));
+            }
+        }
+
+        private AppPrefs Prefs { get; set; }
+
         public MainWindow()
         {
+            var appDataDir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var appDir = IOPath.Combine(appDataDir, "DZT");
+            _prefsJsonPath = IOPath.Combine(appDir, "prefs.json");
+            if (!IODir.Exists(appDir))
+            {
+                IODir.CreateDirectory(appDir);
+            }
+
+            Prefs = new AppPrefs(_prefsJsonPath);
+            if (!IOFile.Exists(_prefsJsonPath))
+            {
+                Prefs.Store();
+            }
+            else
+            {
+                Prefs.Load();
+            }
+
             InitializeComponent();
+            DayzServerRootDirTextBox.Text = Prefs.DayzServerRootDirectoryPath;
+            MpMissionNameTextBox.Text = Prefs.MpMissionName;
         }
 
         private void SetDayzServerRootDirButton_Click(object sender, RoutedEventArgs e)
@@ -32,15 +83,19 @@ namespace DZT.Gui
             if (picker.ShowDialog() is true)
             {
                 DayzServerRootDirTextBox.Text = picker.ResultPath;
+                Prefs.DayzServerRootDirectoryPath = picker.ResultPath;
+                Prefs.Store();
             }
         }
 
         private void SetMpMissionFolderButton_Click(object sender, RoutedEventArgs e)
         {
-            var picker = new FolderPicker { InputPath=DayzServerRootDirTextBox.Text};
+            var picker = new FolderPicker { InputPath = DayzServerRootDirTextBox.Text };
             if (picker.ShowDialog() is true)
             {
                 MpMissionNameTextBox.Text = picker.ResultPath;
+                Prefs.MpMissionName = picker.ResultPath;
+                Prefs.Store();
             }
         }
     }
